db6171cacb44fc114728eea8d69db1bccca8a973
Kano <133855037+kanochiyoo@users.noreply.github.com>
2023-10-15 19:04:11 +0700

    update

--- 5_F.cpp
+++ 5_F.cpp
@@ -1,8 +1,26 @@
-#include <bits/stdc++.h>
-#include <cmath>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    // buntu min
+int main() {
+    double N;
+    int F, C, bilbul;
+    
+    cin >> N;
+    bilbul = N;
+    
+    if (N == bilbul) {
+        F = N;
+        C = N;
+    }
+    else if (N > 0) {
+        F = N;
+        C = N + 1; 
+    } else {
+        F = N - 1;
+        C = N;
+    }
+    
+    cout << F << " " << C;
+
+    return 0;
 }
