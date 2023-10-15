1fe3f7e92e4bd3203c00b25dc7b53a68bc141291
Kano <133855037+kanochiyoo@users.noreply.github.com>
2023-10-15 19:11:13 +0700

    Add files via upload

---
+++ 6_E.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    
+    cin >> N;
+    
+    while (N % 2 == 0) {
+        N = N / 2;
+    }
+    
+    if (N == 1) {
+        cout << "ya";
+    } else {
+        cout << "bukan";
+    }   
+    
+    return 0;
+}
